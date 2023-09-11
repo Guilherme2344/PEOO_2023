bc67f00534d77e4d6db40fb41e29c1f9d6690570
Guilherme Cristiano <124366188+Guilherme2344@users.noreply.github.com>
2023-09-11 10:15:38 -0300

    Update cadastro_atualizado.py

--- cadastro_atualizado.py
+++ cadastro_atualizado.py
@@ -1,131 +1,142 @@
-import json
-
-class Cliente:
-    def __init__(self, id, nome, email, fone):
+import json, datetime, time
+class Servico:
+    def __init__(self, id, desc, valor, duracao):
         self.__id = id
-        self.__nome = nome
-        self.__email = email
-        self.__fone = fone
+        self.__desc = desc
+        self.__valor = valor
+        self.__duracao = duracao
 
     def set_id(self, id):
         self.__id = id
 
-    def set_nome(self, nome):
-        self.__nome = nome
+    def set_desc(self, desc):
+        self.__desc = desc
 
-    def set_email(self, email):
-        self.__email = email
+    def set_valor(self, valor):
+        self.__valor = valor
 
-    def set_fone(self, fone):
-        self.__fone = fone
+    def set_duracao(self, duracao):
+        self.__duracao = duracao
 
     def get_id(self):
         return self.__id
-    
-    def get_nome(self):
-        return self.__nome
-    
-    def get_email(self):
-        return self.__email
-    
-    def get_fone(self):
-        return self.__fone
-    
+
+    def get_desc(self):
+        return self.__desc
+
+    def get_valor(self):
+        return self.__valor
+
+    def get_duracao(self):
+        return self.__duracao
+
     def __str__(self):
-        return f'{self.__id}; {self.__nome}; {self.__email}; {self.__fone}'
+        return f'{self.__id}; {self.__desc}; {self.__valor}; {self.__duracao}'
+
+
+class NServico:
+    __servicos = []
     
+    @classmethod
+    def inserir(cls, servico):
+        NServico.abrir()
+        id = 0
+        for i in cls.__servicos:
+            if i.get_id() > id: id = i.get_id()
+        servico.set_id(id + 1)
+        cls.__servicos.append(servico)
+        NServico.salvar()
 
-class NCliente:
-    def __init__(self):
-        self.__clientes = []
+    @classmethod
+    def listar(cls):
+        NServico.abrir()
+        return cls.__servicos
 
-    def inserir(self, c):
-        self.__clientes.append(c)
-
-    def listar(self):
-        return self.__clientes
-    
-    def listar_id(self, c_id):
-        for cliente in self.__clientes:
-            if cliente.get_id() == c_id:
-                return cliente
+    @classmethod
+    def listar_id(cls, id):
+        NServico.abrir()
+        for i in cls.__servicos:
+            if i.get_id() == id: return i
         return None
-    
-    def atualizar(self, valor):
-        for i in self.__clientes:
-            if i.get_id() == valor.get_id():
-                i.set_id(valor.get_id())
-                i.set_nome(valor.get_nome())
-                i.set_email(valor.get_email())
-                i.set_fone(valor.get_fone())
 
-    def excluir(self, c):
-        for i in self.__clientes:
-            if i.get_id() == c:
-                self.__clientes.remove(i)
+    @classmethod
+    def atualizar(cls, obj):
+        NServico.abrir()
+        servico = NServico.listar_id(obj.get_id())
+        servico.set_desc(obj.get_desc())
+        servico.set_valor(obj.get_valor())
+        servico.set_duracao(obj.get_duracao())
+        NServico.salvar()
 
-    def abrir(self, nome):
+    @classmethod
+    def excluir(cls, obj):
+        NServico.abrir()
+        servico = NServico.listar_id(obj.get_id())
+        cls.__servicos.remove(servico)
+        NServico.salvar()
+
+    @classmethod
+    def abrir(cls):
         try:
-            with open(nome, 'r') as arquivo:
-                self.__clientes = json.load(arquivo)
+            cls.__servicos = []
+            with open('servicos.json', 'r') as arquivo:
+                a = json.load(arquivo)
+                for servico in a:
+                    s = Servico(servico['_Servico__id'], servico['_Servico__desc'], servico['_Servico__valor'], servico['_Servico__duracao'])
+                    cls.__servicos.append(s)
         except FileNotFoundError:
-            print(f'Arquivo {nome} não encontrado.')
+            pass
 
-    def salvar(self, nome):
-        with open(nome, 'w') as arquivo:
-            json.dump(self.__clientes, arquivo, indent=4)
+    @classmethod
+    def salvar(cls):
+        with open('servicos.json', 'w') as arquivo:
+            json.dump(cls.__servicos, arquivo, default=vars)
 
 
 class UI:
-    def menu():
-        print('0 - sair; 1 - inserir; 2 - listar; 3 - atualizar; 4 - excluir')
-        return int(input())
-    
-    def main():
-        op = 10
-        x = NCliente()
+    @classmethod
+    def main(cls):
+        op = 50
         while op != 0:
             op = UI.menu()
-            if op == 1:
-                id = int(input('Id: '))
-                nome = input('Nome: ')
-                email = input('E-mail: ')
-                fone = input('Fone: ')
-                a = Cliente(id, nome, email, fone)
-                x.inserir(a)
-                print('cliente cadastrado')
-            if op == 2:
-                for i in x.listar(): print(i)
-            if op == 3:
-                id = int(input('Id: '))
-                nome = input('Novo nome: ')
-                email = input('Novo e-mail: ')
-                fone = input('Novo fone: ')
-                b = Cliente(id, nome, email, fone)
-                x.atualizar(b)
-                print('cliente atualizado')
-            if op == 4:
-                id = int(input('Id: '))
-                x.excluir(id)
-                print('cliente excluído')
+            if op == 1: UI.ServicoInserir()
+            if op == 2: UI.ServicoListar()
+            if op == 3: UI.ServicoAtualizar()
+            if op == 4: UI.ServicoExcluir()
+
+    @classmethod
+    def menu(cls):
+        print('0 - sair; 1 - inserir; 2 - listar; 3 - atualizar; 4 - excluir')
+        return int(input())
+
+    @classmethod
+    def ServicoInserir(cls):
+        desc = input('Descrição: ')
+        valor = float(input('Valor: '))
+        duracao = time.strftime('%H:%M:%S', time.strptime(input('Duração: '), '%H:%M:%S'))
+        servico = Servico(0, desc, valor, duracao)
+        NServico.inserir(servico)
+
+    @classmethod
+    def ServicoListar(cls):
+        for servico in NServico.listar(): print(servico)
+
+    @classmethod
+    def ServicoAtualizar(cls):
+        UI.ServicoListar()
+        id = int(input('Digite o id a ser atualizado: '))
+        desc = input('Nova descrição: ')
+        valor = float(input('Novo valor: '))
+        duracao = time.strftime('%H:%M:%S', time.strptime(input('Duração: '), '%H:%M:%S'))
+        servico = Servico(id, desc, valor, duracao)
+        NServico.atualizar(servico)
+
+    @classmethod
+    def ServicoExcluir(cls):
+        UI.ServicoListar()
+        id = int(input('Digite o id a ser excluído: '))
+        servico = Servico(id, '', '', '')
+        NServico.excluir(servico)
+
 
 UI.main()
-
-
-@classmethod
-  def abrir(cls):
-    cls.clientes = []
-    try: 
-      with open("clientes.json", mode="r") as arquivo:
-        clientes_json = json.load(arquivo)
-        for obj in clientes_json:
-          #cliente = Cliente(**obj)
-          cliente = Cliente(obj["_id"], obj["_nome"], obj["_email"], obj["_fone"])
-          cls.clientes.append(cliente)
-    except (FileNotFoundError):
-      pass      
-
-  @classmethod 
-  def salvar(cls):
-    with open("clientes.json", mode="w") as arquivo:
-      json.dump(cls.clientes, arquivo, default=lambda obj: obj.__dict__)
